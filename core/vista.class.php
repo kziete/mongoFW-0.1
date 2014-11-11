9a7a308f81389524f48a9a17ffb2eac5d699e297
kziete <sebastian@elmundoalreves.cl>
2014-11-11 18:04:43 -0300

    Fix Vista array vacio

--- core/vista.class.php
+++ core/vista.class.php
@@ -10,18 +10,18 @@ class Vista{
 		$this->m = new Mustacho();
 		$this->m->templateDir .= 'templates/';
 	}
-	public function armar($template,$hash){		
+	public function armar($template,$hash = array()){		
 		$hash['content'] = $this->m->render($template,$hash);
 		echo $this->m->render('contenedor.html',$hash);
 	}
-	public function mostrar($template,$hash){		
+	public function mostrar($template,$hash = array()){		
 		echo $this->m->render($template,$hash);
 	}
 	public function mostrarSinRender($template){
 		echo file_get_contents($this->m->templateDir .$template, FILE_USE_INCLUDE_PATH);
 	}
 
-	public function getModulo($template,$hash){		
+	public function getModulo($template,$hash = array()){		
 		return $this->m->render($template,$hash);		
 	}
 	public function json($data){
